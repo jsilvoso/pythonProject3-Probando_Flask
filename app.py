3ccb8841ab82e89ff2ea744847ccc2689919380f
jsilvoso <jsilvoso@hotmail.com>
2025-02-04 00:10:24 +0100

    Ajustes

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+import requests
+import os
 
 app = Flask(__name__)
+
+# Variables de entorno
+PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN")
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 
 @app.route('/')
 def home():
@@ -13,6 +19,8 @@ def chatbot():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
