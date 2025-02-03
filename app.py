6677ae078dfd01df17c441cc424c929d40284868
jsilvoso <jsilvoso@hotmail.com>
2025-02-04 00:18:37 +0100

    Ajustes

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ def send_message(recipient_id, message_text):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #app.run(host="0.0.0.0", port=5000)
+   # app.run(debug=True)
+   app.run(host="0.0.0.0", port=5000)
